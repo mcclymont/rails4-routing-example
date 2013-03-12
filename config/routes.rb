ef51729909489c15ec89128500220abbe33458f5
Trevor Turk <trevorturk@gmail.com>
2013-03-12 11:23:00 -0500

    Rails 3, app displays /second

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Example::Application.routes.draw do
   root to: 'application#index'
 
-  # get 'first'  => 'application#example', as: :example
-  # get 'second' => 'application#example', as: :example
+  get 'first'  => 'application#example', as: :example
+  get 'second' => 'application#example', as: :example
 end
